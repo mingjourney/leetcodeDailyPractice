c87f830bfe6e2a6bd2cb0bb668189406f4551376
gujiaming898 <gujiaming898@hellobike.com>
2025-03-28 18:54:31 +0800

    feat:

--- 20250328/146.lru-缓存.js
+++ 20250328/146.lru-缓存.js
@@ -9,15 +9,21 @@
  * @param {number} capacity
  */
 var LRUCache = function(capacity) {
-  
-};
-
+  this.capacity = capacity;
+  this.cache = new Map();
+}
 /** 
  * @param {number} key
  * @return {number}
  */
 LRUCache.prototype.get = function(key) {
-
+  if (this.cache.has(key)) {
+    const value = this.cache.get(key);
+    this.cache.delete(key);
+    this.cache.set(key, value);
+    return value;
+  }
+  return -1;
 };
 
 /** 
@@ -26,7 +32,13 @@ LRUCache.prototype.get = function(key) {
  * @return {void}
  */
 LRUCache.prototype.put = function(key, value) {
-
+  if (this.cache.has(key)) {
+    this.cache.delete(key)
+  } else if (this.cache.size >= this.capacity) {
+    const key = this.cache.keys().next().value;
+    this.cache.delete(key);
+  }
+  this.cache.set(key, value);
 };
 
 /** 
